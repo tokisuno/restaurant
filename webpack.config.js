303eb03eb501e038ddd2b9687010ab8a83897641
tokisuno <lucaswmei@gmail.com>
2024-08-06 12:09:00 -0400

    a

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
             },
         ],
     },
-    optimization: {
-        runtimeChunk: 'single',
-    },
+    //optimization: {
+    //    runtimeChunk: 'single',
+    //},
 }
