5e03ff2e0eb0785f80314b2ad6401cf8b7afa662
tokisuno <lucaswmei@gmail.com>
2024-08-20 10:54:45 -0400

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,18 @@ module.exports = {
                     skipEmptyLines: true
                 }
             },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
         ],
     },
     //optimization: {
